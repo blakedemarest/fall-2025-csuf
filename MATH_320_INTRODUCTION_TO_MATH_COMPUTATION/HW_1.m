d45903c406380e33eaf34433739d2284279d6b99
blakedemarest <blakedemarest@csu.fullerton.edu>
2025-08-27 16:58:47 -0700

    ch 1 math 320

---
+++ MATH_320_INTRODUCTION_TO_MATH_COMPUTATION/HW_1.m
@@ -0,0 +1,65 @@
+%PROBLEM 1
+disc = sqrt(4 - 4* (-2*3));
+
+denom = 2 * 3;
+
+negb = -2;
+
+root1 = (negb + disc) / denom
+root2 = (negb - disc) / denom
+
+%PROBLEM 2
+
+a_p1 = 3;
+b_p1 = 2;
+c_p1 = 1;
+
+vertexX = -b/(2*a)
+vertexY = c - b^2 / (4*a)
+
+%PROBLEM 3
+
+monke = pi/5;
+
+trig1 = cos(monke/2)^2
+trig2 = (tan(monke) + sin(monke))/(2*tan(monke))
+
+%PROBLEM 4
+
+T_0 = 120;
+T_S = 38;
+k = 0.45;
+t = 3;
+
+T = T_S + (T_0 - T_S) * exp(-k * t)
+
+% PROBLEM 5
+format bank
+P = 5000;
+r = 0.085;
+n = 1;
+t = 17;
+
+Acc1 = P * (1 + r/n)^(n*t)
+Acc2 = P * (1 + r/(n*12))^((n*12)*t)
+
+% PROBLEM 5
+format default
+
+alpha = 47;
+beta = 43;
+gamma = 180 - beta - alpha;
+
+% calculate sides a b and c using law of sines
+
+
+
+perim = 114;
+
+s = perim / 2;
+
+r = sqrt(((s - sind(alpha) )*(s - sind(beta))*(s-sind(gamma))) / s)
+
+
+
+
